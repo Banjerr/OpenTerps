58283d4327dcb865ebbcabfe58ff19c6ce9a669f
Ben Redden <benjamminredden@gmail.com>
2020-11-14 14:00:42 -0500

    adding new rules for effects/strains/smells/tastes, building for heroku

--- main.go
+++ main.go
@@ -5,8 +5,13 @@ import (
 	"net/http"
 	"os"
 
+	"openterps/categories"
 	"openterps/dbconnector"
+	"openterps/effects"
 	"openterps/migrations"
+	"openterps/smells"
+	"openterps/strains"
+	"openterps/tastes"
 	"openterps/terpenes"
 
 	"github.com/gin-gonic/gin"
@@ -36,6 +41,72 @@ func main() {
 
 	// PUT /terpenes
 	r.PATCH("/terpenes/:id", terpenes.UpdateTerpene)
+
+	// Delete /terpenes
+	r.DELETE("/terpenes/:id", terpenes.DeleteTerpene)
+
+	// GET /effects
+	r.GET("/effects", effects.GetEffects)
+
+	// POST /effects
+	r.POST("/effects", effects.CreateEffect)
+
+	// PUT /effects
+	r.PATCH("/effects/:id", effects.UpdateEffect)
+
+	// Delete /effects
+	r.DELETE("/effects/:id", effects.DeleteEffect)
+
+	// POST /migrations
+	r.POST("/migrations", migrations.RunMigration)
+
+	// GET /tastes
+	r.GET("/tastes", tastes.GetTastes)
+
+	// POST /tastes
+	r.POST("/tastes", tastes.CreateTastes)
+
+	// PUT /tastes
+	r.PATCH("/tastes/:id", tastes.UpdateTastes)
+
+	// Delete /tastes
+	r.DELETE("/tastes/:id", tastes.DeleteTastes)
+
+	// GET /smells
+	r.GET("/smells", smells.GetSmells)
+
+	// POST /smells
+	r.POST("/smells", smells.CreateSmells)
+
+	// PUT /smells
+	r.PATCH("/smells/:id", smells.UpdateSmells)
+
+	// Delete /smells
+	r.DELETE("/smells/:id", smells.DeleteSmells)
+
+	// GET /strains
+	r.GET("/strains", strains.GetStrains)
+
+	// POST /strains
+	r.POST("/strains", strains.CreateStrains)
+
+	// PUT /strains
+	r.PATCH("/strains/:id", strains.UpdateStrains)
+
+	// Delete /strains
+	r.DELETE("/strains/:id", strains.DeleteStrains)
+
+	// GET /categories
+	r.GET("/categories", categories.GetCategories)
+
+	// POST /categories
+	r.POST("/categories", categories.CreateCategories)
+
+	// PUT /categories
+	r.PATCH("/categories/:id", categories.UpdateCategories)
+
+	// Delete /categories
+	r.DELETE("/categories/:id", categories.DeleteCategories)
 
 	// POST /migrations
 	r.POST("/migrations", migrations.RunMigration)
